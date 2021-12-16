d6967139b43410c3d903ad59379232dec4d4ee94
Ismail Mohamud <51711008+iamismaila@users.noreply.github.com>
2021-12-16 02:24:51 -0800

    Delete Python Crash Courses directory

--- Python Crash Courses/Day One/hello.py
+++
@@ -1,9 +0,0 @@
-## Day one 
-## Chapter One - Python concept 
-import this
-#*
-student_name ="ismail"
-univrsity_name = "Kookmin"
-sentence = student_name +" goes to "+ univrsity_name
-msg =sentence
-print(msg.upper())
--- Python Crash Courses/Day Two/module.py
+++
@@ -1,4 +0,0 @@
-from Classes import User_car as u
-
-data =u('model','blue','2020')
-print(data)
